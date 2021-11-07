ecd79a87fc01c12eac401fa253b84b14c39fb72f
Rik <rdoppenberg@gmail.com>
2021-11-07 17:15:53 +0100

    reverting back to mysql

--- mysql/init/metastore.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE IF NOT EXISTS metastore; 
-
-/* create hive user */
-CREATE USER 'hive'@'%' IDENTIFIED WITH mysql_native_password BY 'hive1234';
-GRANT ALL PRIVILEGES ON metastore . * TO 'hive'@'%';
