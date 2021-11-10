4650ae4eb0e237206ab27691bfea90849db6dacf
Rik <rdoppenberg@gmail.com>
2021-11-10 20:57:13 +0100

    Separated static conf into defaults and cleaned up

--- spark-server/entrypoint.sh
+++ spark-server/entrypoint.sh
@@ -1,13 +1,12 @@
 #!/bin/bash
 
-# set -e
+set -e
 echo "Setting Env variables"
 
 # set hive and hadoop standards
 export HIVE_HOME="/opt/hive"
 export HADOOP_HOME="/opt/hadoop"
-# export HADOOP_CLASSPATH="$HADOOP_CLASSPATH:/opt/spark/jars"
-
 export PATH="$HIVE_HOME/bin:$HADOOP_HOME:$PATH"
 
-exec "$@"
+# exec "$@"
+$@
