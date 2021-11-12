366524d3a91360fe0dfe6f97c34a668f2adecfa7
Rik <rdoppenberg@gmail.com>
2021-11-12 18:39:46 +0100

    moved env setting to Dockerfile

--- spark-server/entrypoint.sh
+++ spark-server/entrypoint.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 set -e
-echo "Setting Env variables"
 
-# set hive and hadoop standards
-export HIVE_HOME="/opt/hive"
-export HADOOP_HOME="/opt/hadoop"
-export PATH="$HIVE_HOME/bin:$HADOOP_HOME:$PATH"
+echo "Starting Spark Thriftserver..."
+spark/sbin/start-thriftserver.sh \
+    --master k8s://${KUBERNETES_SERVICE_HOST}:${KUBERNETES_SERVICE_PORT} \
+    --hiveconf javax.jdo.option.ConnectionPassword=${HIVE_USER_PWD} \
+    --conf spark.driver.host=$(hostname -I) \
+    --packages com.google.cloud.bigdataoss:gcs-connector:hadoop3-2.2.0 
 
-# exec "$@"
-$@
+echo "Spark Thriftserver running at port 10000"
+
+# TODO: change to elegant solution
+tail -F /dev/null
